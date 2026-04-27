1fa7c65de1a87fa7c3b556019e8d67afe2cb20b4
paseflo76 <paseflo34@gmail.com>
2026-04-27 10:25:49 +0200

    fix mongodb connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
 //TODO: Conectar a la base de datos
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URL)
-    console.log('conectado con la BBDD👍🚀')
+
+    console.log('conectado con la BBDD 👍🚀')
   } catch (error) {
-    console.log('error conectando con la BBDD')
+    console.error('ERROR CONECTANDO BBDD:', error.message)
+    console.error('STACK:', error.stack)
+
+    process.exit(1)
   }
 }
 
